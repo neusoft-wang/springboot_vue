875c5361501f8b1cd4173616a698a83b6dbb29db
wang <live609450148@qq.com>
2018-01-03 22:42:11 +0800

    学习并整理js文件

--- src/main/java/com/boylegu/springboot_vue/controller/StudentController.java
+++ src/main/java/com/boylegu/springboot_vue/controller/StudentController.java
@@ -16,18 +16,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/Student")
